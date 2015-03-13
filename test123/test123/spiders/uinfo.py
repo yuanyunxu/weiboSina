0c22b79018fda46ad19a6e934fe89965ff7bd081
yuanyunxu <317551593@qq.com>
2015-03-13 11:55:23 +0800

    Change the source of code

--- test123/test123/spiders/uinfo.py
+++ test123/test123/spiders/uinfo.py
@@ -1,5 +1,5 @@
 #coding:utf-8
-import os,re,time
+import os,re,time,random
 from scrapy.http import Request
 from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
@@ -43,11 +43,41 @@ class UinfoSpider(CrawlSpider):
         print self.item['follower_num']
         self.item['user_description'] = response.select('//div[@class="ut"]/span[@style]/text()').extract()
         print self.item['user_description']
+
+
+        #self.item['blog_content'] = []
+        #tmpBlogContent = response.select('//div[@id]/span[@class="ctt"]/text()').extract()
+        #self.item['blog_content'].extend(tmpBlogContent)
+        self.item['blog_forward_content'] = []
+        tmpBlogForwardContent = response.select('//span[@class="cmt"]/following-sibling::span[@class="ctt"]/text()').extract()
+        self.item['blog_forward_content'].extend(tmpBlogForwardContent)
+        try:
+            totalNum = int(re.sub('1/|页','',response.select('//div[@id="pagelist"]/form/div/text()').extract()[0]))
+            self.item['pageNum'] = 50 if totalNum >= 50 else totalNum
+        except:
+            self.item['pageNum'] = 0
         return Request("http://weibo.cn/account/privacy/tags/?uid="+str(self.item['user_id']),cookies = self.cookieDict,callback = self.parse2)
     
     def parse2(self,response):
         response = HtmlXPathSelector(response)
         self.item['user_tags'] = response.select('//div[@class="c"][3]/a/text()').extract()
         print self.item['user_tags']
-        time.sleep(30)
-        return self.item
+        time.sleep(random.uniform(5,60))
+        for k in xrange(2,self.item['pageNum']+1):
+            yield Request("http://weibo.cn/?uid="+str(self.item['user_id']+'?page='+str(k)),cookies = self.cookieDict,callback = self.parseContent)
+    
+    def parseContent(self.response):
+        response = HtmlXpathSelector(response)
+        #tmpBlogContent = response.select('//div[@id]/span[@class="ctt"]/text()').extract()
+        #self.item['blog_content'].extend(tmpBlogContent)
+        tmpBlogForwardContent = response.select('//span[@class="cmt"]/following-sibling::span[@class="ctt"]/text()').extract()
+        self.item['blog_forward_content'].extend(tmpBlogForwardContent)
+
+        try:
+            tmpBlogNum = len(response.select('//div[@id]'))
+        except:
+            tmpBlogNum = 0
+        for blogId in xrange(1,tmpBlogNum+1):
+
+        
+
